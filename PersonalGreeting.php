218551b98ca2277b4d90a6ab7865da70511b4180
DimitriPantzos <dpantzos@dattobackup.com>
2014-04-17 15:40:14 -0400

    Personal Greeting  Form
    This is a Personal Greeting Form
    Made with PHP, and HTML

---
+++ PersonalGreeting.php
@@ -0,0 +1,29 @@
+<?php
+//dpantzos
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1'); 
+
+if( !empty($_POST['name']) ) {
+    $message= "<h1>Greetings, {$_POST['name']}, and welcome.</h1>";
+} 
+
+?>
+
+<html>
+<head>
+	<title>Personalized Greeting Form</title> 
+<head> 
+	<body>
+<?php 
+	if( isset($message) ) {
+		echo $message;
+	} 
+?>
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+			Enter your name: <input type="text" name="name" />
+			<input type="submit" />
+		</form>		
+	</body>
+</html>
+	
